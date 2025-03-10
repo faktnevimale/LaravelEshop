adad2600edb6f6213815235c505557c1ef8d18f3
Sofia <barbora.stavova@outlook.cz>
2025-03-10 08:45:19 +0100

    n

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,71 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 import preset from './vendor/filament/support/tailwind.config.preset'
+const Unfonts = require("unplugin-fonts");
 
 /** @type {import('tailwindcss').Config} */
 export default {
     presets: [preset],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
         },
+        colors: {
+            'blue': '#1fb6ff',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+        },
+        fontFamily: {
+            "Bonny" : ["Bonny"]
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
     },
-
     plugins: [
-        forms,
-        require('@tailwindcss/line-clamp'),
+        require("@tailwindcss/typography"),
+        Unfonts.default.vite({
+            custom: {
+                families: [
+                    {
+                        name: "Neco",
+                        local: "Neco",
+                        src: "./public/fonts/Neco-Bold.ttf",
+                    },
+                    {
+                        name: "Bonny",
+                        local: "Bonny",
+                        src: "./public/fonts/Bonny-Bold.ttf",
+                    },
+                    {
+                        name: "ClashDisplay",
+                        local: "ClashDisplay",
+                        src: "./public/fonts/ClashDisplay-Bold.ttf",
+                    },
+                ],
+            },
+        }),
     ],
 };
